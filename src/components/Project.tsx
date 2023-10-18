304124db8d18d2464ea4291e8bb068d427787344
Blzs101 <95141082+Blzs101@users.noreply.github.com>
2023-10-18 12:45:34 +0200

    added extra responsibility

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,9 +9,9 @@ type PropsType = {
 function Project({ link, image, title, tech }: PropsType) {
 
     return (
-        <Link href={link} className=" p-4 rounded-xl  dark:bg-slate-700 bg-slate-300  lg:hover:scale-110 lg:ease-in-out lg:duration-700  ">
-            <Image src={image} className="p-2 rounded-3xl w-full h-[80%]" alt="website preview" width={800} height={800} loading="eager" priority />
-            <h2 className="pb-2 pl-2 font-extrabold md:text-xl">{title}</h2>
+        <Link href={link} className=" p-4 rounded-xl  dark:bg-slate-700 bg-slate-300  md:hover:scale-110 md:ease-in-out md:duration-700  ">
+            <Image src={image} className="p-2 rounded-3xl w-full h-[75%]" alt="website preview" width={800} height={800} loading="eager" priority />
+            <h2 className="mt-4 pb-2 pl-2 font-extrabold md:text-xl">{title}</h2>
             <div className="pb-2 pl-2 font-bolt md:text-mg">{tech}</div>
         </Link>
     )
